6cb9c29580e7e3e72bc8b2279064e6e3b5023790
YangYi <368315960@qq.com>
2018-10-22 16:33:07 +0800

    修改包名

---
+++ psop-admin/src/main/java/com/fhzz/psop/service/user/impl/UserInfoServiceImpl.java
@@ -0,0 +1,23 @@
+package com.fhzz.psop.service.user.impl;
+
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fhzz.psop.dao.jpa.UserInfoDao;
+import com.fhzz.psop.entity.UserInfo;
+import com.fhzz.psop.service.UserInfoService;
+
+@RestController
+@RequestMapping("/user")
+public class UserInfoServiceImpl implements UserInfoService {
+	@Resource
+	private UserInfoDao userInfoDao;
+
+	@Override
+	@RequestMapping("/findByUsername")
+	public UserInfo findByUsername(String username) {
+		return userInfoDao.findByUsername(username);
+	}
+}
---
+++ psop-entity/src/main/java/com/fhzz/psop/PsopEntityApplication.java
@@ -0,0 +1,12 @@
+package com.fhzz.psop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsopEntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsopEntityApplication.class, args);
+	}
+}
